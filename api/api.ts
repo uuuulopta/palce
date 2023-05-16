71499cf9a3004972a20ffba17f88971d2d8d6809
Ivan <uuuulopta@gmail.com>
2023-05-16 13:25:43 +0200

    Setup inserting pixels at backend and refactored mongodb code.

--- api/api.ts
+++ api/api.ts
@@ -1,39 +1,31 @@
 import {Express, Request, Response,} from "express"
 import express = require("express");
-import {  MongoClient, ServerApiVersion } from "mongodb";
+import {uri,client,Pixel,pixels,insertColor}  from "./db";
 import {logger} from "./loggingSetup"
 import dotenv = require('dotenv')
+import { buffer } from "stream/consumers";
 dotenv.config()
 
-
-interface Pixel{
-color: String;
-x: Number;
-y: Number;
+async function parseInput(color: any, x: any, y: any, callback: Function){
+    const hexRegex = new RegExp("[0-9A-F]{6}")
+    if(typeof color !== "string") throw new Error("Color is not a string!")
+        if(typeof x !== "string") throw new Error("X is not a string!")
+        if(typeof y !== "string") throw new Error("Y is not a string!")
+        if(color && x && y){
+            if(!hexRegex.test(color)) throw Error("Bad color value!");
+            if(!RegExp( "[0-9]+" ).test(x)) throw Error("x is not a number!")
+            if(!RegExp( "[0-9]+" ).test(y)) throw Error("y is not a number!")
+            await callback(color,x,y)
+    }
 }
 const app: Express = express()
 const port = 300
 
-const uri = "mongodb://127.0.0.1:27017";
-const client = new MongoClient(uri,  {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        }
-    }
-);
 
 const hexToBase64 = function(hex: String) {return Buffer.from(hex, 'hex').toString('base64') }
 const Base64ToHex = function(base64: String) {return Buffer.from(base64, 'base64').toString('hex') }
 const run = async () => {
 await client.connect().then(() => {logger.info(`Connected to ${uri} `)})
-.catch((err) => {logger.error(`Failed to conenct to ${err.message}`)});
-}
-
-run();
-const pixels = client.db("place").collection("pixels");
-const hexRegex = new RegExp("[0-9A-F]{6}")
 // pixels.insertOne({value: hexToBase64("FFFFFF"),x: 0, y: 0});
 //const docs = pixels.find({},{projection: {_id: 0}});
 //for await (const doc of docs){
@@ -43,20 +35,11 @@ console.log("Path = " + "/" + process.env.API_URL_PREFIX +  '/api')
   app.get(( "/" + process.env.API_URL_PREFIX +  '/api' ).replace("//","/"), async (req: Request, res: Response) => {
   try{
     console.log("GET")
-    if(typeof req.query.color !== "string") throw new Error("Color is not a string!")
-    if(typeof req.query.x !== "string") throw new Error("X is not a string!")
-    if(typeof req.query.y !== "string") throw new Error("Y is not a string!")
-    if(req.query.color && req.query.x && req.query.y){
-        if(!hexRegex.test(req.query.color)) throw Error("Bad color value!");
-        if(!RegExp( "[0-9]+" ).test(req.query.x)) throw Error("x is not a number!")
-        if(!RegExp( "[0-9]+" ).test(req.query.y)) throw Error("y is not a number!")
-
-
-        const obj: Pixel = {color: req.query.color, x: Number( req.query.x ),
-         y: Number( Number( req.query.y ) )}
-        res.send(obj)
-    }
-    
+    parseInput(req.query.color,req.query.x,req.query.y,async (color: String,x: String,y: String) => {
+        await insertColor(color,x,y);
+        const pixelsArray = await pixels.find({}).toArray()
+        res.send(pixelsArray);
+    })
   }
   catch (error){
     logger.error(error.message)
@@ -67,4 +50,5 @@ console.log("Path = " + "/" + process.env.API_URL_PREFIX +  '/api')
   app.listen(port, () => {
     logger.info("Listening on port 300")
   });
-
+}
+ run();
---
+++ api/db.ts
@@ -0,0 +1,24 @@
+
+import {  MongoClient, ServerApiVersion, WithId, Document} from "mongodb";
+export const uri = "mongodb://127.0.0.1:27017";
+export const client = new MongoClient(uri,  {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    }
+);
+export interface Pixel{
+color: Buffer;
+x: Number;
+y: Number;
+}
+
+export const pixels = client.db("place").collection("pixels");
+export async function insertColor(color: String,x: String,y: String): Promise<void> {
+var bdata = Buffer.from(color,"hex"); 
+const obj: Pixel = {color: bdata, x: Number( x ),
+y: Number( Number( y ) )}
+await pixels.updateOne({x: obj.x,y: obj.y},{ $set: obj },{upsert: true});
+}
