3855516d058464e177b0e37872ef27b99215867f
Ivan <uuuulopta@gmail.com>
2023-05-25 21:52:54 +0200

    Changed insertcolor parameters. Added exportfield function.

--- api/db.ts
+++ api/db.ts
@@ -1,4 +1,3 @@
-
 import {  MongoClient, ServerApiVersion, WithId, Document} from "mongodb";
 export const uri = "mongodb://127.0.0.1:27017";
 export const client = new MongoClient(uri,  {
@@ -16,10 +15,11 @@ y: Number;
 }
 
 export const pixels = client.db("place").collection("pixels");
-export async function insertColor(color: String,x: String,y: String): Promise<void> {
-    var bdata = Buffer.from(color,"hex"); 
-    const obj: Pixel = {color: bdata, x: Number( x ),y: Number( Number( y ) )}
+export async function insertColor(color: Buffer,x: number,y: number): Promise<void> {
+    const obj: Pixel = {color: color, x:  x ,y:   y  }
     await pixels.updateOne({x: obj.x,y: obj.y},{ $set: obj },{upsert: true});
 }
-// temporary function for testing todo: remove
+export async function exportFieldRedis(){
+   return await pixels.find({}).toArray();
+}
 
