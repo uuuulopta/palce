7576532c8cc26043e62766890bfc2f5d4b7c7a56
Ivan <uuuulopta@gmail.com>
2023-05-25 23:06:20 +0200

    Added better way of initializing fields.

--- api/redis.ts
+++ api/redis.ts
@@ -1,5 +1,6 @@
 import { createClient,commandOptions } from 'redis';
 import { logger } from './loggingSetup';
+import  {WithId,BSON} from "mongodb"
 
 function colorBufferToInt(color: Buffer,bytes:number): number{
 const buf = Buffer.alloc(bytes)
@@ -18,6 +19,7 @@ await client.connect();
 }
 export async function setColor(x: number, y: number, color: Buffer){
     const offset = getOffset(x,y);
+    console.log(x,y)
     const colorInt: number = colorBufferToInt(color,4)
      client.bitField("field",[{
         operation: "SET",
@@ -42,8 +44,11 @@ export async function setColorTransaction(x: number, y: number, color: Buffer,tr
 
 }
 export async function readField(){
+    console.log("reading field")
    const field = await client.get(commandOptions({returnBuffers:true}),"field");
-   //TODO: if field is empty transfer mongodb to redis and read field again
+   
+   //TODO: if field ismpty transfer mongodb to redis and read field again
+   console.log("returning")
     if(field == null) return Buffer.from( Uint8ClampedArray.from([255,255,255,255]) )
     return Buffer.from( Uint8ClampedArray.from(field) ) 
 }
@@ -51,15 +56,36 @@ export async function readField(){
 // TODO: find a better solution
 // when it gets connected to mongodb it should just pull data from it
 export async function initializeEmptyField(){
+    //const transaction = client.multi()
+    //for(var x = 0;x<500;x++){
+    //    for(var y = 0;y<500;y++){
+    //       setColorTransaction(x,y,Buffer.from("FFFFFFFF","hex"),transaction)
+    //    }
+
+    //}
+    // await transaction.exec(true);
+    setColor(499,499,Buffer.from("FFFFFFFF","hex"))
+
+}
+
+export async function importMongo(data: WithId<BSON.Document>[]){
     const transaction = client.multi()
-    for(var x = 0;x<500;x++){
-        for(var y = 0;y<500;y++){
-           setColorTransaction(x,y,Buffer.from("FFFFFFFF","hex"),transaction)
+    let pixel : WithId<BSON.Document> | undefined = data.shift();
+    let y_max: number = 0;
+    let x_max: number = 0;
+    while(typeof pixel != "undefined"){
+        const x: number = pixel.x 
+        const y: number = pixel.y 
+        const color: Buffer = pixel.color.buffer
+        setColorTransaction(x,y,color,transaction)
+        pixel = data.shift() 
+        if(y > y_max){
+            y_max = y;
+            x_max = x;
         }
-
+        else if(y == y_max && x > x_max) x_max = x; 
     }
-      transaction.exec(true);
-
-
+    if(y_max != 499 && x_max != 499) setColorTransaction(499,499,Buffer.from("FFFFFFFF","hex"),transaction)
+    await transaction.exec(true);
 }
 
