37415a7638cc04ef81016fb663ae819dbfee3c1e
Ivan <uuuulopta@gmail.com>
2023-05-28 18:48:42 +0200

    Refactored mongodb into a class

--- api/db.ts
+++ api/db.ts
@@ -1,26 +1,33 @@
-import {  MongoClient, ServerApiVersion, WithId, Document} from "mongodb";
+import {  MongoClient, ServerApiVersion} from "mongodb";
+import { logger as loggerp } from "./loggingSetup";
 import "dotenv/config"
-export const uri = process.env.MONGO_URI;
-export const client = new MongoClient(uri,  {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        }
-    }
-);
 export interface Pixel{
-color: Buffer;
-x: Number;
-y: Number;
+        color: Buffer;
+        x: Number;
+        y: Number;
+    }
+export class Mongo{
+    logger;
+    constructor(logger_name:string){
+        this.logger = loggerp.child({parent:logger_name,caller:"mongo"}) 
+    }
+    uri = process.env.MONGO_URI;
+    client = new MongoClient(this.uri,  {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+            }
+        }
+    );
+    pixels = this.client.db("place").collection("pixels");
+    async insertColor(color: Buffer,x: number,y: number): Promise<void> {
+        const obj: Pixel = {color: color, x:  x ,y:   y  }
+        await this.pixels.updateOne({x: obj.x,y: obj.y},{ $set: obj },{upsert: true});
+        this.logger.debug(`Set pixel ${obj}`)
+    }
+     async exportField(){
+       this.logger.debug("Exported field")
+       return await this.pixels.find({}).toArray();
+    }
 }
-
-export const pixels = client.db("place").collection("pixels");
-export async function insertColor(color: Buffer,x: number,y: number): Promise<void> {
-    const obj: Pixel = {color: color, x:  x ,y:   y  }
-    await pixels.updateOne({x: obj.x,y: obj.y},{ $set: obj },{upsert: true});
-}
-export async function exportField(){
-   return await pixels.find({}).toArray();
-}
-
