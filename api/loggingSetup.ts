c309161a6364d0bb69c82c8aa4bc97c5e1608a02
Ivan <uuuulopta@gmail.com>
2023-05-28 18:33:28 +0200

    Changed logging format

--- api/loggingSetup.ts
+++ api/loggingSetup.ts
@@ -1,19 +1,24 @@
-import winston = require('winston'); 
+
+const winston = require('winston');
 const logger = winston.createLogger({
     level: 'info',
-    format: winston.format.json(),
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf((info:any,)=> `${info.timestamp} [${info.parent}] ${info.level}: ${info.message} | ${info.caller} `)
+
+    ),
     transports: [
-      //
-      // - Write all logs with importance level of `error` or less to `error.log`
-      // - Write all logs with importance level of `info` or less to `combined.log`
-      //
-      new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'combined.log' }),
+        //
+        // - Write all logs with importance level of `error` or less to `error.log`
+        // - Write all logs with importance level of `info` or less to `combined.log`
+        //
+        new winston.transports.File({ filename: 'error.log', level: 'error',format:winston.format.json()}),
+        new winston.transports.File({ filename: 'combined.log',format:winston.format.json()}),
     ],
-  });
-  if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-      format: winston.format.simple(),
-    }));
-  }
+});
+ if (process.env.NODE_ENV !== 'production') {
+   logger.add(new winston.transports.Console({
+     level: "debug",
+   }));
+ }
 export {logger}
